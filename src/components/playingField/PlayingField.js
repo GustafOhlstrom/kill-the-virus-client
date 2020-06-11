8b3b573b081e491d6afbad8bb81688270f084b4c
Gustaf Ohlstrom <gustaf_ohlstrom@hotmail.com>
2020-06-11 18:51:49 +0200

    Update Scoreboard table syntax and Added coundown to start game in PlayingField

--- src/components/playingField/PlayingField.js
+++ src/components/playingField/PlayingField.js
@@ -1,17 +1,67 @@
 import './playingField.scss'
+import virusA from '../../assets/virus-icons/a.svg'
 import React from 'react'
 
 class PlayingField extends React.Component {
-    state = {}
+    state = {
+        countDown: 1,
+        startGame: false,
+        newGame: true,
+        virusIcon: null,
+    }
 
+    componentDidMount() {
+        this.newGame()
+    }
+
+    handleOnClick = () => {
+        console.log("faaaast")
+    }
+    
+    displayVirus = () => {
+        this.setState({
+            virusIcon: <img 
+                className="virus-icon"
+                src={virusA} 
+                alt="kill it" 
+                style={{ top: "50%", left: "50%" }}
+                onClick={this.handleOnClick}
+            />
+        })
+    }
+
+    newRound = () => {
+        const { newGame } = this.state
+
+        // Check if new game and set countdown timer accordingly
+        newGame
+            ? this.setState({ newGame: false })
+            : this.setState({ countDown: 3 })
+
+        const roundCountDownId = setInterval(() => {
+            if(this.state.countDown <= 1) {
+                clearInterval(roundCountDownId)
+                this.setState({ startGame: true })
+                this.displayVirus()
+            }
+            this.setState(state => ({ countDown: state.countDown - 1 }))
+        }, 1000);
+    }
+
+    newGame = () => {
+        this.newRound()
+    }
+    
     render() {
+        const { countDown, startGame, virusIcon } = this.state
+
         return (
-            <section id="playingField">
+            <section id="playing-field">
                 {/* Countdown for next round */}
-                <div className="next-round">
-                    3
-                </div>
-                <canvas></canvas>
+                {!startGame
+                    ? <div className="next-round">{ countDown }</div>
+                    : <div className="board">{ virusIcon }</div>
+                }
             </section>
         )
     }
