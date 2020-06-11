b0d361108062a87436b0f6266dbd98c771663ca2
Gustaf Ohlstrom <gustaf_ohlstrom@hotmail.com>
2020-06-11 18:50:03 +0200

    Added a lobby for registration and connected app with socket.IO

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 import './main.scss'
 import React from 'react';
+import Lobby from './components/lobby/Lobby'
 import PlayingField from './components/playingField/PlayingField'
 import Scoreboard from './components/scoreboard/Scoreboard'
 
-function App() {
-	return (
-		<main id="app">
-			<h1 className="title">Kill The Virus</h1>
-			<div className="flex-container">
-				<PlayingField />
-				<Scoreboard />
-			</div>
-		</main>
-	);
+import socketIOClient from 'socket.io-client';
+
+class App extends React.Component{
+
+	state = {
+		socket: null,
+		user: null,
+	}
+
+	componentDidMount() {
+		const socket = socketIOClient('http://localhost:3000');
+		socket.emit('register-user', (status) => {
+			console.log("Server acknowledged the registration :D", status);
+		});
+
+		this.setState({ socket })
+	}
+
+	registerUser = username => this.setState({ user: username })
+
+	render() {
+		const { user, socket } = this.state
+		return (
+			<main id="app">
+				<h1 className="title">Kill The Virus</h1>
+				<div className="flex-container">
+					{user
+						? <>
+							<PlayingField />
+							<Scoreboard />
+						</>
+						: <Lobby 
+							socket={socket}
+							registerUser={username => this.registerUser(username)}
+						/>}
+				</div>
+			</main>
+		)
+	}
 }
 
 export default App;
---
+++ src/components/lobby/Lobby.js
@@ -0,0 +1,45 @@
+import './lobby.scss'
+import React from 'react'
+
+class Lobby extends React.Component {
+    state = {
+        username: null,
+    }
+
+    handleChange = e => this.setState({ [e.target.id]: e.target.value })
+
+    handleSubmit = e => {
+        e.preventDefault()
+        
+        const { username } = this.state
+        this.props.socket.emit('user-register', username, (status) => {
+            console.log(`Server acknowledged the registration os user: ${username}`, status);
+            
+            this.props.registerUser(username)
+		});
+    }
+
+    render() {
+        return (
+            <section id="lobby">
+                <form 
+                    className="register"
+                    onSubmit={this.handleSubmit}
+                >
+                    <div className="username">
+                        <label htmlFor="username">
+                            Username
+                        </label>
+                        <input 
+                            id="username"
+                            type="text"
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                </form>
+            </section>
+        )
+    }
+}
+
+export default Lobby
