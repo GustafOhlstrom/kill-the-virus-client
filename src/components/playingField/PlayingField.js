aea23f5ab737210351d8a532af05821642a79648
Gustaf Ohlstrom <gustaf_ohlstrom@hotmail.com>
2020-06-17 20:54:06 +0200

    PlayingField made easier to comprehend, more if-statements less ternary-statements

--- src/components/playingField/PlayingField.js
+++ src/components/playingField/PlayingField.js
@@ -5,31 +5,45 @@ import React from 'react'
 function PlayingField(props) {
     const { countdown, virusIcon, winner, roundNr, roundWinner, opponent, user, scores } = props
 
+    let boardContent;
+    if(opponent) {
+        if(countdown) {
+            // Display countdown
+            boardContent = <div className="next-round">{ countdown }</div>
+        } else if(winner) {
+            // Display game winner
+            boardContent = (
+                <div className="next-round"> 
+                    <p>{ winner }</p>
+                    <p>{ winner !== "Victory by Surrender" && scores[user.id] + " - " + scores[opponent.id] }</p>  
+                </div>
+            )
+        } else if(roundWinner) {
+            // Display round winner
+            boardContent = (
+                <div className="next-round"> 
+                    <p>{ roundWinner === "Draw" ? "Draw" : (roundWinner=== user.name ? "You Won" : "You Lost") }</p>
+                    <p className="winning-round">Round { roundNr + 1 }</p>
+                </div>
+            )
+        } else {
+            // Display virus
+            boardContent = <div className="virus-icon-area">{ virusIcon }</div>
+        }
+    } else {
+        // Display loader and looking for opponent text
+        boardContent = (
+            <div className="waiting-for-opponent">
+                <p>Looking for an opponent</p>
+                <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+            </div>
+        )
+    }
+
     return (
         <section id="playing-field">
             <div className="board">
-                {opponent 
-                    ? countdown
-                        ? <div className="next-round">{ countdown }</div>
-                        : winner || roundWinner 
-                            ? <div className="next-round">
-                                {winner 
-                                    ? <>
-                                        <p>{ winner }</p>
-                                        <p>{ winner !== "Victory by Surrender" && scores && scores[user.id] + " - " + scores[opponent.id] }</p>
-                                    </>
-                                    : <>
-                                        <p>{ roundWinner === "Draw" ? "Draw" : (roundWinner=== user.name ? "You Won" : "You Lost") }</p>
-                                        <p className="winning-round">Round { roundNr + 1 }</p>
-                                    </> 
-                                }
-                            </div>
-                            : <div className="virus-icon-area">{ virusIcon }</div> 
-                    : <div className="waiting-for-opponent">
-                        <p>Looking for an opponent</p>
-                        <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-                    </div>
-                } 
+                { boardContent }
             </div>
         </section>
     )
